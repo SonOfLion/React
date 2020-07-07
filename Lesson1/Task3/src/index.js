8dc121ebcd0f394f0dc62e8649afe076e7312a1f
SonOfLion <syplusplus@gmail.com>
2020-07-07 15:44:35 +0300

    3

--- Lesson1/Task3/src/index.js
+++ Lesson1/Task3/src/index.js
@@ -10,8 +10,8 @@ const element = ( <
     div className = "greeting" >
     <
     div className = "greeting__title" > Hello, world! < /div> <
-    div className = "greeting__text" > I 'm learning React</div>  <
-    /div>
+    div className = "greeting__text" > I 'm learning React</div>  < /
+    div >
 );
 
 // console.log(element);

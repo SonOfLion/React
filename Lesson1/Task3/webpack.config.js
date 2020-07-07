d9d89ed3ba593552eeaf6a94f4b2cd03cb325b7b
SonOfLion <syplusplus@gmail.com>
2020-07-07 14:38:58 +0300

    3

---
+++ Lesson1/Task3/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + '/review_build',
+    },
+    module: {
+        rules: [{
+                test: /.js$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}

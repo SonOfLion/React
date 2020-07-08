f50bc1904dadbdc12067f5b8a41f30f289cfc882
SonOfLion <syplusplus@gmail.com>
2020-07-08 16:05:01 +0300

    1

--- Lesson3/Task1/src/index.jsx
+++ Lesson3/Task1/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchField from './Search.jsx';
+import Search from './Search.jsx';
 import './index.scss';
 import './search.scss';
 

25b67195dbf14d676a28242f20acd38c787c744c
SonOfLion <syplusplus@gmail.com>
2020-07-10 20:05:23 +0300

    5

--- Lesson3/HW2/src/Profile.jsx
+++ Lesson3/HW2/src/Profile.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import moment from 'moment';
+import {Component} from 'react';
 
-const Profile = (props) => {
-    const ren = moment(props.user.birthDate).format("DD MMM YY");
+class Profile extends Component {
+    render(){
+    const ren = moment(this.props.user.birthDate).format("DD MMM YY");
 
     return (
         <div className="profile">
             <div className="prifile__name">
-                {`${props.user.firstName} ${props.user.lastName}`}
+                {`${this.props.user.firstName} ${this.props.user.lastName}`}
             </div>
             <div className="profile__birth">
-                {`Was born ${ren} in ${props.user.birthPlace}`}
+                {`Was born ${ren} in ${this.props.user.birthPlace}`}
             </div>
         </div>
     );
+    };
 };
 
 export default Profile;

00c2a0a6f4f94b16ad5d7eb97a198572ab122cff
SonOfLion <syplusplus@gmail.com>
2020-07-10 21:58:47 +0300

    test

--- Lesson3/HW2/src/App.jsx
+++ Lesson3/HW2/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Profile from './profile';
+import React from "react";
+import Profile from './Profile'
 
 const user = {
-    firstName:'John',
-    lastName:'Doe',
-    birthDate:'1991-01-17T11:11:11.819Z',
-    birthPlace:'London'
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London'
 };
 
 const App = () => {
-    return (<Profile userData={user}/>);
-};
-
-export default App;
+    return (
+        <Profile userData = {user}/>
+        )
+}
+    
+export default App; 
--- Lesson3/HW2/src/Profile.jsx
+++ Lesson3/HW2/src/Profile.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import moment from 'moment';
-import {Component} from 'react';
+import moment from "moment";
+import {Component} from "react";
 
-class Profile extends Component {
-    // console.log(props);
-    render(){
-    const ren = moment(this.props.userData.birthDate).format("DD MMM YY");
-    // console.log(props);
-    return (
-        <div className="profile">
-            <div className="prifile__name">
-                {`${this.props.userData.firstName} ${this.props.userData.lastName}`}
+class Profile extends Component { 
+    
+    render() {
+        console.log(this.props);
+        const age = moment(this.props.userData.birthDate).format("DD MMM YY");
+        
+        return (
+            <div className="profile">
+                <div className="profile__name">
+                    {`${this.props.userData.firstName} ${this.props.userData.lastName}`}
+                </div>
+                <div className="profile__birth">
+                    {`Was born ${age} in ${this.props.userData.birthPlace}`}
+                </div>
             </div>
-            <div className="profile__birth">
-                {`Was born ${ren} in ${this.props.userData.birthPlace}`}
-            </div>
-        </div>
-    );
+        );
     };
-};
-
+}
 export default Profile;

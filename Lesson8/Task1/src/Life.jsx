fc3e30d798796aee2e7abd5b7ae5295c46a3fe7b
SonOfLion <syplusplus@gmail.com>
2020-07-19 14:03:16 +0300

    1

--- Lesson8/Task1/src/Life.jsx
+++ Lesson8/Task1/src/Life.jsx
@@ -17,7 +17,7 @@ class Life extends Component {
     };
 
     componentDidUpdate(prevProps, prevState){
-        console.log('componentDidUpdate(prevProps, prevState): some updates on new props');
+        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
 
     componentWillUnmount(){

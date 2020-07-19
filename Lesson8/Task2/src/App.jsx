e9331876aa29d743f7d9b741b64574a0046ee6a0
SonOfLion <syplusplus@gmail.com>
2020-07-19 16:32:26 +0300

    2

--- Lesson8/Task2/src/App.jsx
+++ Lesson8/Task2/src/App.jsx
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import Clock from './Clock';
 
 class App extends Component {
-    state = {
-    visible: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: true,
+        };
     };
 
     toggle = () => {
-    this.setState({
-        visible: !this.state.visible
-    });
+        this.setState({
+            visible: !this.state.visible
+        });
     };
 
     render() {
-    return (
-        <div className="worltime">
-            <button onClick={this.toggle}>Toggle</button>
-            <div>{this.state.visible && <Clock location='New York' offset={-5} />}</div>
-            <div>{this.state.visible && <Clock location='Kyiv' offset={2} />}</div>
-            <div>{this.state.visible && <Clock location='London' offset={0} />}</div>
-        </div>
-    );
+        return (
+            <div className="worltime">
+                <button className="toggle" onClick={this.toggle}>Toggle</button>
+                {this.state.visible && <Clock location ={'New York'} offset ={-5}/>}
+                {this.state.visible && <Clock location={'Kyiv'} offset={2} />}
+                {this.state.visible && <Clock location ={'London'} offset ={0}/>}
+            </div>
+        );
     };
 };
 

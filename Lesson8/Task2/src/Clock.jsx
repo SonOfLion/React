25b7fb0e005dbb688f4cb3800b672472f56ca07b
SonOfLion <syplusplus@gmail.com>
2020-07-19 17:07:31 +0300

    2

--- Lesson8/Task2/src/Clock.jsx
+++ Lesson8/Task2/src/Clock.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 export  const  getTimeWithOffset = offset =>{
     const currentTime = new Date();
-    const utcOffset = currentTime.getTimezoneOffset()/60;
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
     return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
 };
 

002f9bffa1d265907cb2794b5f8982ca4f5d38fe
SonOfLion <syplusplus@gmail.com>
2020-07-19 19:17:04 +0300

    3

--- Lesson8/Task3/src/Dimensions.jsx
+++ Lesson8/Task3/src/Dimensions.jsx
@@ -32,7 +32,7 @@ class Dimensions extends Component{
     render(){
         return (
         <div className="dimensions">
-            {`${this.state.width}px - ${this.state.height}`}
+            {`${this.state.width}px - ${this.state.height}px`}
         </div>
         );
     };

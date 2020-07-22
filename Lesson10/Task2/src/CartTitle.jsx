e1a1d41e651ab77253e9dc49c55a31c4a490323b
SonOfLion <syplusplus@gmail.com>
2020-07-22 23:33:09 +0300

    2

---
+++ Lesson10/Task2/src/CartTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartTitle = ({userName, count}) => {
+    return (
+        <h1 className="cart-title">
+            {`${userName}, you added ${count} items`}
+        </h1>
+    );
+};
+
+export default CartTitle;

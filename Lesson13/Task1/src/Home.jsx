4526989b44166e2b44c524c9d65dfdd267d47023
SonOfLion <syplusplus@gmail.com>
2020-07-29 16:06:49 +0300

    1

--- Lesson13/Task1/src/Home.jsx
+++ Lesson13/Task1/src/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Home () {
+const Home = () => {
     return(
         <div className="page__content">
             <h1>🏠</h1>

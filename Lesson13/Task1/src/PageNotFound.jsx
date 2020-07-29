31e99fa2eeb5af571717ca55d49de01232dfd91b
SonOfLion <syplusplus@gmail.com>
2020-07-29 16:01:39 +0300

    1

--- Lesson13/Task1/src/PageNotFound.jsx
+++ Lesson13/Task1/src/PageNotFound.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Contacts = () => {
     return (
         <div className="page__content">
-            <h1>😢</h1>
+            <h1>404 😢</h1>
             <a href="/">Go home</a>
         </div>
     );

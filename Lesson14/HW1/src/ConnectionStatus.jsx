832aee7b76d210bce992e247109671149227b020
SonOfLion <syplusplus@gmail.com>
2020-08-02 17:06:52 +0300

    1

---
+++ Lesson14/HW1/src/ConnectionStatus.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+function ConnectionStatus () {
+    const [status, setStatus] = useState('online');
+
+    useEffect(() => {
+        const statusOnline = () => {
+            setStatus('online');
+        };
+
+        const statusOffline = () => {
+            setStatus('offline');
+        };
+    
+        window.addEventListener('online', statusOnline);
+        window.addEventListener('offline', statusOffline);
+    
+        return () => {
+            window.removeEventListener('online', statusOnline)
+            window.removeEventListener('offline', statusOffline)
+        };
+    
+    }, [status])
+
+    return(
+        <div className="status">
+            {status ? 'online' : 'offline' }
+        </div>
+    );
+};
+
+export default ConnectionStatus;

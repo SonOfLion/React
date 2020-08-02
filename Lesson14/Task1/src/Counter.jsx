15927a7789c2787269c2978c3ffef4f7ea85e982
SonOfLion <syplusplus@gmail.com>
2020-08-02 14:16:46 +0300

    1

--- Lesson14/Task1/src/Counter.jsx
+++ Lesson14/Task1/src/Counter.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 
 const Counter = () => {
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
 
     useEffect(() => {
     }, [counter]);
@@ -11,13 +11,13 @@ const Counter = () => {
     return (
         <div className="counter">
             <button className="counter__button" onClick={() => setCounter(counter - 1)}>
-            -
+                -
             </button>
             <span className="counter__value" onClick={() => setCounter(0)}>
             {counter}
             </span>
             <button className="counter__button" onClick={() => setCounter(counter + 1)}>
-            +
+                +
             </button>
         </div>
     );

b0b2b1a8abe3b9c957bf2c330d51fac73b4969c7
SonOfLion <syplusplus@gmail.com>
2020-08-02 15:05:46 +0300

    2

--- Lesson14/Task2/src/App.jsx
+++ Lesson14/Task2/src/App.jsx
@@ -6,25 +6,25 @@ import User from './User';
 const App = () => {
     return (
         <div className="page">
-        <Router>
-        <div className="page__content">
-                <h1>Users</h1>
-            <ul className="navigation">
-                <li className="navigation__item">
-                    <Link to="/users/github">Github</Link>
-                </li>
-                <li className="navigation__item">
-                    <Link to="/users/facebook">Facebook</Link>
-                </li>
-            </ul>
-            <Switch>
-                <Route path="/users/:userId" component={User}></Route>
-                <Route path="/" >
-                    <span>Select a user please</span>
-                </Route>
-            </Switch>
-        </div>
-        </Router>
+            <Router>
+                <div className="page__content">
+                        <h1>Users</h1>
+                    <ul className="navigation">
+                        <li className="navigation__item">
+                            <Link to="/users/github">Github</Link>
+                        </li>
+                        <li className="navigation__item">
+                            <Link to="/users/facebook">Facebook</Link>
+                        </li>
+                    </ul>
+                    <Switch>
+                        <Route path="/users/:userId" component={User}></Route>
+                        <Route path="/" >
+                            <span>Select a user please</span>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </div>
     );
 };

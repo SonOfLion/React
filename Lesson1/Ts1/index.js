28c33df81ce5bf33be4640c0db37bd19388abc6c
SonOfLion <syplusplus@gmail.com>
2020-09-25 00:28:13 +0300

    1

---
+++ Lesson1/Ts1/index.js
@@ -0,0 +1,9 @@
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+    'div',
+    { class: 'greeting' },
+    'Hello, React!'
+);
+
+ReactDOM.render(greetingElem, rootElement);

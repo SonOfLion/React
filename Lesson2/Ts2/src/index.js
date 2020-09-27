18074ba41e0de770dc617a4ceba172c81f0214ce
SonOfLion <syplusplus@gmail.com>
2020-09-27 06:27:28 +0300

    HW-Todo-list

--- Lesson2/Ts2/src/index.js
+++ Lesson2/Ts2/src/index.js
@@ -4,34 +4,16 @@ import '../src/index.css'
 
 const rootElement = document.querySelector('#root');
 
-const renderSeconds = time => {
-    
-    const seconds = new Date(time).getSeconds();
-
-    const backgroundColor = seconds % 2 === 0
-        ? '#fff'
-        : '#000';
-
-    const textColor = seconds % 2 !== 0
-        ? '#fff'
-        : '#000';
-
-    const styles = {
-            color: textColor,
-            backgroundColor,
-        };
-    
-    const element = (
-            <div className="seconds"
-                style={styles}
-            >
-                {seconds}
+const element = (
+    <>
+            <h1 className="title">Todo List</h1>
+        <main className="todo-list">
+            <div className="actions">
+                <input className="task-input" type="text"/>
+                <button className="btn create-task-btn">Create</button>
             </div>
-        );
+        </main>
+    </>
+);
 
-    ReactDOM.render(element, rootElement);
-};
-
-renderSeconds(new Date());
-
-setInterval(()=> renderSeconds(new Date()),1000); 
+ReactDOM.render(element, rootElement);

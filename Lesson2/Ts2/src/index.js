721dc48c25499a8d0c31f68ece3f1703ec76b0a0
SonOfLion <syplusplus@gmail.com>
2020-09-27 05:51:11 +0300

    3

--- Lesson2/Ts2/src/index.js
+++ Lesson2/Ts2/src/index.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../src/index.css'
 
 const rootElement = document.querySelector('#root');
 
-const element = (
-<main className="page">
-<form className="login-form">
-    <h1 className="form-title">Profile</h1>
-    <div className="form-control">
-        <label className="form-label" htmlFor="name">Name</label>
-        <input
-            className="form-input"
-            type="text"
-            id="name"
-            name="name"
-            value="anonymous"
-        />
-    </div>
-    <div className="form-control">
-        <label className="form-label" id="age" htmlFor="age">Age</label>
-        <input
-            className="form-input"
-            type="number"
-            value="17"
-            name="age"
-        />
-        <span style={{color: 'red', fontWeight: 700}}>To young</span>
-    </div>
-    <button className="submit-button" type="submit">Submit</button>
-</form>
-</main>
-);
+const renderSeconds = time => {
+    
+    const seconds = new Date(time).getSeconds();
 
-ReactDOM.render(rootElement,element);
+    const backgroundColor = seconds % 2 === 0
+        ? '#fff'
+        : '#000';
+
+    const textColor = seconds % 2 !== 0
+        ? '#fff'
+        : '#000';
+
+    const styles = {
+            color: textColor,
+            backgroundColor,
+        };
+    
+    const element = (
+            <div className="seconds"
+                style={styles}
+            >
+                {seconds}
+            </div>
+        );
+
+        ReactDOM.render(element, rootElement);
+};
+
+renderSeconds(new Date());
+
+setInterval(()=> renderSeconds(new Date()),1000); 

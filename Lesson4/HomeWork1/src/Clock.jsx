d22d148b9d4720f225aed154f4660600ddde436c
SonOfLion <syplusplus@gmail.com>
2020-10-02 12:07:18 +0300

    1

---
+++ Lesson4/HomeWork1/src/Clock.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './clock.scss';
+import moment from 'moment';
+
+const getTimeWithOffset = offset => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+}
+
+class Clock extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            location: props.location,
+            time:moment(getTimeWithOffset(this.props.offset)).format("h:mm:ss A")
+        };
+
+        setInterval(()=> {
+            this.setState({
+                time:moment(getTimeWithOffset(this.props.offset)).format("h:mm:ss A")
+            });
+        },1000);
+    };
+
+    render() {
+        return (
+            <div className="clock">
+                <div className="clock__location">
+                    {this.state.location}
+                </div>
+                <div className="clock__time">
+                    {this.state.time}
+                </div>
+            </div>
+            
+            
+        );
+    };
+};
+
+export default Clock;
+
+
+// const getTimeWithOffset = offset => {
+//     const currentTime = new Date();
+//     const utcOffset = currentTime.getTimezoneOffset() / 60;
+//     return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+// }

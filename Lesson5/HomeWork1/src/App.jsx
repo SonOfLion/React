db8e53aecc6d715c1d5ceea9a3e147b9f352e6dd
SonOfLion <syplusplus@gmail.com>
2020-10-04 07:39:13 +0300

    1

---
+++ Lesson5/HomeWork1/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Toggler from './Toggler';
+
+function App () {
+    return (
+        <Toggler />
+    );
+};
+
+export default App;
---
+++ Lesson5/HomeWork1/src/Toggler.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './toggler.scss';
+
+class Toggler extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {button: 'Off'};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => ({
+            button:!state.button
+        }))
+        console.log(this.state.button)
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick} className="toggler">
+                {this.state.button ? 'Off' : 'On'}
+            </button>
+        );
+    };
+};
+
+
+export default Toggler;
